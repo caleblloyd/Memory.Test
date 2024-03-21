728700480cf056730d7b1734521830c04e491992
Safayat Borhan <SAFBO@orsted.com>
2024-03-22 07:57:42 +0800

    Add project files.

---
+++ Memory.Test.Legacy/Program.cs
@@ -0,0 +1,48 @@
+﻿using NATS.Client.JetStream;
+using NATS.Client;
+using System.Diagnostics;
+
+Options opts = ConnectionFactory.GetDefaultOptions("localhost");
+ConnectionFactory connectionFactory = new ConnectionFactory();
+var conn = connectionFactory.CreateConnection(opts);
+IJetStream jetStream = conn.CreateJetStreamContext();
+IJetStreamManagement jetStreamManagement = conn.CreateJetStreamManagementContext();
+
+jetStreamManagement.AddStream(StreamConfiguration.Builder()
+                .WithName("EVENTS")
+                .WithStorageType(StorageType.File)
+                .WithSubjects("events.>")
+                .Build());
+
+var tasks = new List<Task>();
+for (int i = 0; i < 1000; i++)
+{
+    var task = Task.Run(() =>
+    {
+        while (true)
+        {
+            var sw = Stopwatch.StartNew();
+            for (var i = 0; i < 2; i++)
+            {
+                jetStream.PublishAsync(subject: "events.page_loaded", data: null);
+                jetStream.PublishAsync(subject: "events.mouse_clicked", data: null);
+                jetStream.PublishAsync(subject: "events.mouse_clicked", data: null);
+                jetStream.PublishAsync(subject: "events.page_loaded", data: null);
+                jetStream.PublishAsync(subject: "events.mouse_clicked", data: null);
+                jetStream.PublishAsync(subject: "events.input_focused", data: null);
+            }
+
+            //PrintStreamStateAsync(jetStreamManagement.GetStreamInfo("EVENTS"));
+            Console.WriteLine($"Total time taken: {sw.Elapsed.TotalSeconds}");
+        }
+    });
+    tasks.Add(task);
+}
+
+await Task.WhenAll(tasks);
+
+void PrintStreamStateAsync(StreamInfo jsStream)
+{
+    var state = jsStream.State;
+    Console.WriteLine($"Stream has {state.Messages} messages using {state.Bytes} bytes");
+}
---
+++ Memory.Test/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Logging;
+using NATS.Client.Core;
+using NATS.Client.JetStream;
+using NATS.Client.JetStream.Models;
+using System.Diagnostics;
+
+var natsOptions = NatsOpts.Default;
+natsOptions = natsOptions with { Url = "localhost" };
+await using var nats = new NatsConnection(natsOptions);
+var js = new NatsJSContext(nats);
+var config = new StreamConfig(name: "EVENTS", subjects: new[] { "events.>" });
+config.Storage = StreamConfigStorage.File;
+var stream = await js.CreateStreamAsync(config);
+
+var tasks = new List<Task>();
+for (int i = 0; i < 1000; i++)
+{
+    var task = Task.Run(async () =>
+    {
+        while (true)
+        {
+            var sw = Stopwatch.StartNew();
+            for (var i = 0; i < 2; i++)
+            {
+                await js.PublishAsync<object>(subject: "events.page_loaded", data: null);
+                await js.PublishAsync<object>(subject: "events.mouse_clicked", data: null);
+                await js.PublishAsync<object>(subject: "events.mouse_clicked", data: null);
+                await js.PublishAsync<object>(subject: "events.page_loaded", data: null);
+                await js.PublishAsync<object>(subject: "events.mouse_clicked", data: null);
+                await js.PublishAsync<object>(subject: "events.input_focused", data: null);
+            }
+
+            //await PrintStreamStateAsync(stream);
+            Console.WriteLine($"Total time taken: {sw.Elapsed.TotalSeconds}");
+        }
+    });
+    tasks.Add(task);
+}
+
+await Task.WhenAll(tasks);
+
+async Task PrintStreamStateAsync(INatsJSStream jsStream)
+{
+    await jsStream.RefreshAsync();
+    var state = jsStream.Info.State;
+    Console.WriteLine($"Stream has {state.Messages} messages using {state.Bytes} bytes");
+}
+
+
+
+
+
